b187347bf64a3984ab7b240073fd036d2a923686
KHemamal <khemamal@innosoftwareworld.com>
2020-12-30 07:55:36 +0530

    Added response for the API.

---
+++ aws-kenesis-data-publisher/src/main/java/com/amazonaws/lambda/constants/WebConstants.java
@@ -0,0 +1,12 @@
+package com.amazonaws.lambda.constants;
+
+public class WebConstants {
+	
+	private WebConstants() {}
+	public static final String RESPONSE_HEADER_CUSTOM_VALUE = "x-custom-header";
+	public static final String RESPONSE_BODY_PARAMETER_MESSAGES_FAILED = "messages-failed";
+	public static final String RESPONSE_STATUS = "statusCode";
+	public static final String RESPONSE_HEADERS = "headers";
+	public static final String RESPONSE_BODY = "body";
+	
+}
--- aws-kenesis-data-publisher/src/main/java/function/KenesisDataPublisher.java
+++ aws-kenesis-data-publisher/src/main/java/function/KenesisDataPublisher.java
@@ -3,13 +3,18 @@ package function;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.amazonaws.lambda.constants.ApplicationConstants;
 import com.amazonaws.lambda.constants.EnvironmentConstants;
+import com.amazonaws.lambda.constants.WebConstants;
 import com.amazonaws.lambda.kenesis.model.Message;
 import com.amazonaws.services.kinesis.AmazonKinesis;
 import com.amazonaws.services.kinesis.AmazonKinesisClientBuilder;
@@ -17,9 +22,12 @@ import com.amazonaws.services.kinesis.model.DescribeStreamRequest;
 import com.amazonaws.services.kinesis.model.DescribeStreamResult;
 import com.amazonaws.services.kinesis.model.PutRecordsRequest;
 import com.amazonaws.services.kinesis.model.PutRecordsRequestEntry;
+import com.amazonaws.services.kinesis.model.PutRecordsResult;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 import software.amazon.awssdk.regions.Region;
 
@@ -36,12 +44,16 @@ public class KenesisDataPublisher implements RequestStreamHandler {
 	}
 
 	@Override
-	public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
+	public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
 		final LambdaLogger lambdaLogger = context.getLogger();
 		validateStream(amazonKinesisClient, kenesisStreamName, lambdaLogger);
 		final PutRecordsRequest putRecordsRequest = new PutRecordsRequest();
 		putRecordsRequest.setStreamName(kenesisStreamName);
 		final List<PutRecordsRequestEntry> putRecordsRequestEntryList = new ArrayList<>();
+		final JsonObject responseJson = new JsonObject();
+		JsonObject headerJson = new JsonObject();
+		headerJson.addProperty(WebConstants.RESPONSE_HEADER_CUSTOM_VALUE, "my custom header value");
+		final JsonObject responseBody = new JsonObject();
 		for (int i = 0; i < 10; i++) {
 			final Message message = new Message.MessageBuilder(Long.valueOf(i), "Test payload count :" + i)
 					.addCreatedBy("Admin").addCreatedOn(new Date()).build();
@@ -51,7 +63,25 @@ public class KenesisDataPublisher implements RequestStreamHandler {
 			putRecordsRequestEntryList.add(putRecordsRequestEntry);
 		}
 		putRecordsRequest.setRecords(putRecordsRequestEntryList);
-		amazonKinesisClient.putRecords(putRecordsRequest);
+		PutRecordsResult putRecordsResult = amazonKinesisClient.putRecords(putRecordsRequest);
+		if (putRecordsResult.getFailedRecordCount() > 0) {
+			final List<String> errorSequenceNumbers = putRecordsResult.getRecords().stream()
+					.filter(putRecordsResultEntry -> putRecordsResultEntry.getErrorCode() != null)
+					.map(putRecordsResultEntry -> putRecordsResultEntry.getSequenceNumber())
+					.collect(Collectors.toList());
+			responseBody.addProperty(WebConstants.RESPONSE_BODY_PARAMETER_MESSAGES_FAILED,
+					String.join(",", errorSequenceNumbers));
+			responseJson.addProperty(WebConstants.RESPONSE_STATUS, 500);
+		} else {
+			responseBody.addProperty(WebConstants.RESPONSE_BODY_PARAMETER_MESSAGES_FAILED,
+					Collections.emptyList().toString());
+			responseJson.addProperty(WebConstants.RESPONSE_STATUS, 200);
+		}
+		responseJson.add(WebConstants.RESPONSE_HEADERS, headerJson);
+		responseJson.addProperty(WebConstants.RESPONSE_BODY, responseBody.toString());
+		final OutputStreamWriter writer = new OutputStreamWriter(outputStream, StandardCharsets.UTF_8);
+		writer.write(new Gson().toJson(responseJson));
+		writer.close();
 	}
 
 	private void validateStream(AmazonKinesis kinesisClient, String streamName, LambdaLogger lambdaLogger) {
