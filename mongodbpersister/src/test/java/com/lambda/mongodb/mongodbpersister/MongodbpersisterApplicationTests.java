e68be6a7993518925afde3b7f9d4fb79c5c5a2e7
KHemamal <khemamal@innosoftwareworld.com>
2021-01-03 07:18:37 +0530

    Added the intial commit for mongo db persister.'

---
+++ mongodbpersister/src/test/java/com/lambda/mongodb/mongodbpersister/MongodbpersisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambda.mongodb.mongodbpersister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbpersisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
